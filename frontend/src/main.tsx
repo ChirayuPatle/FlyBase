354d64d9394d097a24af774d48dcd24052d31230
ChirayuPatle <chirayupatle2@gmail.com>
2025-04-27 20:42:13 +0530

    add-layout

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+<<<<<<< HEAD
 import './App.css'
 // import App from './App.tsx'
+=======
+import App from './App.tsx'
+>>>>>>> ed816aed017c7f8f56e317db39a023066bb556db
 import router from './router';
 import { RouterProvider } from 'react-router-dom';
 
