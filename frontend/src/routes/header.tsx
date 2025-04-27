9a0dbbcf4f3229262f811b56703abfa51e75ba36
ChirayuPatle <chirayupatle2@gmail.com>
2025-04-27 20:26:11 +0530

    added header and set bg

--- frontend/src/routes/header.tsx
+++ frontend/src/routes/header.tsx
@@ -16,7 +16,6 @@ const Header: React.FC = () => {
       <div className="text-2xl font-bold text-white">
         FlyBase
       </div>
-
       <div className="flex items-center gap-6">
 
         <button className="bg-green-600 hover:bg-green-700 text-white font-semibold text-sm py-2 px-4 rounded-lg flex items-center space-x-2">
