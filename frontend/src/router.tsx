41330c1cde7ed50b7700dc1eb5ab5318a53c52d5
ChirayuPatle <chirayu.patle@vectorlab.in>
2025-04-27 20:18:33 +0530

    add-header

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,23 +1,31 @@
 import { createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
+
+// Importing all the page components
 import LandingPage from './routes/landing/landing';
 import LoginPage from './routes/auth/login';
 import SignupPage from './routes/auth/signup';
 import Dashboard from './routes/dashboard/dashboard';
+import Deploy from './routes/dashboard/deploy';
+import Overview from './routes/dashboard/overview';
+import Storage from './routes/dashboard/storage';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
+      {/* Public Routes */}
       <Route path='/' element={<LandingPage />} />
-
       <Route path='/login' element={<LoginPage />} />
       <Route path='/signup' element={<SignupPage />} />
 
+      {/* Dashboard Routes (Protected/Nested) */}
       <Route path='/dashboard' element={<Dashboard />}>
-        {/* <Route index element={<Overview />} /> */}
-        {/* <Route path='deploy' element={<Deploy />} /> */}
-        {/* <Route path='storage' element={<Storage />} /> */}
-        {/* <Route path='domain' element={<Domain />} /> */}
-        {/* <Route path='setting' element={<Setting />} /> */}
+        {/* Index route: when only /dashboard is accessed */}
+        <Route index element={<Overview />} />
+
+        {/* Child routes inside dashboard */}
+        <Route path='overview' element={<Overview />} />
+        <Route path='deploy' element={<Deploy />} />
+        <Route path='storage' element={<Storage />} />
       </Route>
     </>
   )
